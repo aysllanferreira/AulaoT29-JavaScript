9b1dd0ca4b9b0a48dd8de5fe022dc606737b9f33
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-12-02 13:18:19 -0300

    teoria 1 e 2 feita

--- Teoria/00-Variaveis.js
+++ Teoria/00-Variaveis.js
@@ -1,2 +1,9 @@
 /* eslint-disable no-console */
 // Variaveis
+
+const nome = 'Pedro';
+let idade = 25;
+
+idade = 28;
+
+console.log(nome, idade);
--- Teoria/01-Operacoes.js
+++ Teoria/01-Operacoes.js
@@ -2,13 +2,35 @@
 // Operações
 
 // Adição
+console.log(2 + 2 + 4);
 
 // Subtração
+const banana = 8;
+const uva = 4;
+const subtracao = banana - uva;
+console.log(subtracao);
 
 // Multiplicação
+const numero = 8;
+const multiplicador = 2;
+console.log(numero * multiplicador);
 
 // Divisão
+const numero2 = 8;
+const divisor = 2;
+const total = numero2 / divisor;
+console.log(total);
 
 // Módulo
+const pares = 10 % 2;
+console.log(pares);
+
+const impar = 13 % 2;
+console.log(impar);
 
 // Exponenciação
+
+const base = 4;
+const expoente = 2;
+const total2 = base ** expoente;
+console.log(total2);
