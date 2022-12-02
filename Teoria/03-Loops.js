59d0e57b9dce6d8a1a43220d6dc3089709d20ca5
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-12-02 14:04:29 -0300

    Strings terminado

--- Teoria/03-Loops.js
+++ Teoria/03-Loops.js
@@ -12,13 +12,27 @@
 // }
 
 // For loop
-for (let index = 0; index <= 10; index += 1) {
-  console.log(index);
-}
+// for (let index = 0; index <= 10; index += 1) {
+//   console.log(index);
+// }
 
 // For of loop
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5, 6];
+// const novoArray = [];
 
-for (const numeros of array) {
-  console.log(numeros * 3);
+// for (const numeros of array) {
+//   if (numeros % 2 === 0) {
+//     novoArray.push(numeros);
+//   }
+// }
+
+// console.log(novoArray);
+
+const array = [1, 2, 3, 4, 3, 6];
+
+for (let index = 0; index <= array.length; index += 1) {
+  if (array[index] === 3) {
+    console.log(array[index]);
+    break;
+  }
 }
--- Teoria/04-Strings.js
+++ Teoria/04-Strings.js
@@ -1,4 +1,12 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-template */
 
-// Strings Literals
+// Template Literals
+
+const nome = 'Rosilene';
+const sobrenome = 'Silva';
+
+const boasvindas1 = 'Ola, seja muito bem-vinda, ' + nome + ' ' + sobrenome + '.';
+console.log(boasvindas1);
+const boasvindas2 = `Ola, seja muito bem-vinda, ${nome} ${sobrenome}.`;
+console.log(boasvindas2);
