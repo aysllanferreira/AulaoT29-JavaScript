1fd7ca48f64d2f9c04bff582e707b7ca35af5abf
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-12-02 13:43:40 -0300

    Loops feitos

--- Teoria/03-Loops.js
+++ Teoria/03-Loops.js
@@ -4,7 +4,21 @@
 // Loops
 
 // While loop
+// let numero = 0;
+
+// while (numero <= 10) {
+//   console.log(numero);
+//   numero += 1;
+// }
 
 // For loop
+for (let index = 0; index <= 10; index += 1) {
+  console.log(index);
+}
 
 // For of loop
+const array = [1, 2, 3, 4, 5];
+
+for (const numeros of array) {
+  console.log(numeros * 3);
+}
